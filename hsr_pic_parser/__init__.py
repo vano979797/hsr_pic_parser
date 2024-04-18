fecefdf5de4e62138aa97c001e01a168f59434c0
Ivan <vano979797@gmail.com>
2024-04-18 23:26:36 +0200

    add prydwen

--- hsr_pic_parser/__init__.py
+++ hsr_pic_parser/__init__.py
@@ -1,6 +1,7 @@
 from .character_picture import get_char_pic, get_char_icon_pic
 from .get_cone import get_cone_pic, get_cone_icon_pic
 from .get_relic import get_relic_icon_pic
+from .prydwen import get_picp
 
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     'get_cone_pic',
     'get_cone_icon_pic',
     'get_relic_icon_pic',
+    'get_picp',
 ]
---
+++ hsr_pic_parser/prydwen.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_picp(name):
+    response = requests.get('https://www.prydwen.gg/star-rail/characters')
+
+    if response.status_code == 200:
+        html_content = response.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        pictures = soup.find_all('img')
+        for picture in pictures:
+            if picture['alt'] == name:
+                pic_url = "https://www.prydwen.gg"+ picture['data-src']
+                maybe_pic = requests.get(pic_url)
+                if maybe_pic.status_code == 200:
+                    return maybe_pic.content, name
+                else:
+                    print('Error loading the image:', maybe_pic.status_code)
+    elif response.status_code == 500:
+        print('Error loading the page:', response.status_code)
